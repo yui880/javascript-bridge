9c204d92205ea1db688297bf6467c03d49dc5a04
yui880 <kimyuna521@gmail.com>
2023-12-09 13:45:36 +0900

    feat(BridgeGameController): 이동할 칸을 입력받아서 이동시키는 기능 구현

--- src/controller/BridgeGameController.js
+++ src/controller/BridgeGameController.js
@@ -1,5 +1,6 @@
 import InputView from '../view/InputView.js';
 import Bridge from '../model/Bridge.js';
+import BridgeGame from '../model/BridgeGame.js';
 
 class BridgeGameController {
   #bridgeGame;
@@ -7,12 +8,22 @@ class BridgeGameController {
   async play() {
     const bridgeSize = await this.#getBridgeSize();
     const bridge = new Bridge(bridgeSize);
+    this.#bridgeGame = new BridgeGame(bridge);
+
+    const movingTile = await this.#getMoving();
+    this.#bridgeGame.move(movingTile);
   }
 
   async #getBridgeSize() {
     const bridgeSize = await InputView.readBridgeSize();
 
     return Number(bridgeSize);
+  }
+
+  async #getMoving() {
+    const moving = await InputView.readMoving();
+
+    return moving;
   }
 }
 
