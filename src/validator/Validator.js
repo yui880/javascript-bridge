0103f39a93d1096ed8fc75e17bde7ca958cdba96
yui880 <kimyuna521@gmail.com>
2023-12-09 15:01:02 +0900

    feat(Validator): 다리의 길이가 범위를 벗어나는 경우 예외 처리 구현

--- src/validator/Validator.js
+++ src/validator/Validator.js
@@ -1,10 +1,17 @@
 import ValidationError from './ValidationError.js';
 import { ERROR } from '../constant/message.js';
+import { BRIDGE_SIZE } from '../constant/bridge.js';
 
 const Validator = {
   checkIsInteger(input) {
     if (!Number.isInteger(input)) {
       throw new ValidationError(ERROR.isNotInteger);
+    }
+  },
+
+  checkValidSize(input) {
+    if (Number(input) < BRIDGE_SIZE.min || Number(input) > BRIDGE_SIZE.max) {
+      throw new ValidationError(ERROR.invalidBridgeSize);
     }
   },
 };
